4927448bdbd8b16b099d90661977a865e4b5b025
cfcodefans <cf.chenfan@gmail.com>
2020-04-12 10:03:54 +0800

    ..

--- statistic-study/with-python/tmp-1.py
+++ statistic-study/with-python/tmp-1.py
@@ -1,5 +1,23 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.axes._axes as Ax
+import matplotlib.figure as Fig
 import numpy as np
 import pandas as pd
 
-t = np.arange(0, 10, 0.1)
-print(t)
+
+def i(*vs):
+    for v in vs:
+        print(f"{type(v)}\t{str(v)}")
+
+
+fig: Fig.Figure = plt.figure(figsize=(2, 2))
+# fig:Fig.Figure = plt.figure(figsize=(1,1))
+ax: Ax.Axes = fig.add_axes([-1, -1, 2, 2])
+ax.set_title("title")
+ax.set_xlabel("x-label")
+ax.set_xlim(left=-1, right=1)
+ax.set_ylabel("y-label")
+ax.set_ylim(bottom=-1, top=1)
+i(ax, ax.title, ax.get_title())
+plt.plot()
